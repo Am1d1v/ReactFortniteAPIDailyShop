7714b81d99c83388b945210c5e79bcee8029c12a
Am1d1v <dimadiman1adima@gmail.com>
2024-03-17 13:14:27 +0300

    .

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import {API_KEY, API_URL} from '../config'
+import { useState, useEffect } from 'react';
+
 
 function Shop() {
+
+  // Initial Goods Array
+  const [goods, setGoods] = useState([]);
+
+  // Loading state
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL, {
+      headers: {
+        'Authorization': API_KEY
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      data.featured && setGoods(data.featured)
+      setLoading(false)
+    })
+  }, []);
+
+  
+
   return (
-    <main className="container content">Shop Content</main>
+    <main className="container content">{goods.map((good) => <h1>{good.name}</h1>)}</main>
   )
 }
 
