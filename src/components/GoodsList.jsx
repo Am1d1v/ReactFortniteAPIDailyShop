a25bacec2fb2bb9cc37d90b6f72d981a507972b2
Am1d1v <dimadiman1adima@gmail.com>
2024-03-17 15:08:32 +0300

    GoodsItem Styles Added

--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -3,10 +3,17 @@ import GoodsItem from './GoodsItem';
 
 function GoodsList(props) {
 
-    const {goods = []} = props;
+  const {goods = []} = props;
+
+  // If we didnt get goods data
+  if (!goods.length){
+    return <h3>Data not found</h3>
+  }
 
   return (
-    goods.map((good) => <GoodsItem {...good}/>)
+    <div className="goods">
+      {goods.map((good) => <GoodsItem {...good}/>)}
+    </div>
   )
 }
 
